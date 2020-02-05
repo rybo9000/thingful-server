740fa02a3d8cb5316ad4092c9363321bf0f8a889
Ryan Fielder <ryan.fielder@hotmail.com>
2020-02-05 16:07:53 -0600

    added Basic Authentication methods

---
+++ src/middleware/basic-auth.js
@@ -0,0 +1,41 @@
+function requireAuth(req, res, next) {
+    
+    const authToken = req.get('Authorization') || ''
+
+    let basicToken
+    
+    // CHECK FOR BASIC TOKEN
+    if (!authToken.toLowerCase().startsWith('basic')) {
+        return res.status(401).json({ error: 'Missing basic token'})
+    } else {
+
+        basicToken = authToken.slice('basic '.length, authToken.length)
+    }
+
+    // DECODE AND CHECK THAT USERNAME AND PASSWORD VALUES EXIST
+    const [tokenUserName, tokenPassword] = Buffer.from(basicToken, 'base64').toString().split(':')
+
+    if (!tokenUserName || !tokenPassword) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+
+    // QUERY DATABASE TO SEE IF USER EXISTS
+    req.app.get('db')('thingful_users')
+        .where({ user_name: tokenUserName })
+        .first()
+        .then(user => {
+            
+            if (!user || user.password !== tokenPassword) {
+                return res.status(401).json({ error: 'Unauthorized reqest'})
+            }
+
+            req.user = user
+
+            next()
+        })
+        .catch(next)
+}
+
+module.exports = {
+    requireAuth
+}
